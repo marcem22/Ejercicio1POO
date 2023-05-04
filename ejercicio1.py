faeeae9bd6964193beb3d95491e8f4f2d2c1b6fe
mMancini <marcelamancinidiaz@gmail.com>
2023-05-04 18:29:37 -0300

    listo

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,4 @@
-import csv
+import csv,re
 class email():
     __idCuenta=""
     __dominio=""
@@ -16,6 +16,11 @@ class email():
     def modificarContraseña(self,contraseña):
         self.__contraseña=contraseña
         return self.__contraseña
+
+def crearCuenta(correos="",contraseña=""):
+	    datos=correos.split("@")
+	    dom=datos[1].split(".")
+	    return email(datos[0],dom[0],dom[1],contraseña)
         
     
 if __name__=="__main__":
@@ -34,9 +39,17 @@ if __name__=="__main__":
     print(f"idCuenta: "+email.__idCuenta+  " - dominio:"+email.__dominio+  " - tipoDominio:"+email.__tipoDominio)"""
     archivo = open('lista.csv')
     reader = csv.reader(archivo,delimiter=';')
+    correos=[]
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$'
     for fila in reader:
-        lineacompleta=fila
-        print(lineacompleta)
-        print("tu mama ")
+        if re.search(email_regex,fila[0]):
+            print(f"correo electronico valido: {fila[0]}")
+            correos.append(crearCuenta(fila[0]))
+        else:
+            print(f"cuenta no valida: {fila[0]}")
+
+        
+
+    
   
 
